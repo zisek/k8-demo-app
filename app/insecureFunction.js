3bb608aee5e306b487680fabf166bf991bf5c24a
zisek <martin.zich@gmail.com>
2024-04-12 11:19:23 +0200

    Update insecureFunction.js

--- app/insecureFunction.js
+++ app/insecureFunction.js
@@ -3,6 +3,9 @@
 // It can be used to test SAST tools' ability to identify sensitive information exposure.
 function insecureFunction(password) {
   console.log("Received password: " + password);
+
+  var db = window.openDatabase("myDb", "1.0", "Personal secrets stored here", 2*1024*1024);  // Noncompliant Web SQL database
+
 }
 var userInput = "sensitivePassword";
 var KEY_TOKEN = "thisisasecrettokenthatnobodyshouldsee";
