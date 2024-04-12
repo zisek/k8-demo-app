4acc2fd003e8889c3fd967695214bcceebd97e9b
zisek <martin.zich@gmail.com>
2024-04-12 18:26:53 +0200

    Update insecureFunction.js

--- app/insecureFunction.js
+++ app/insecureFunction.js
@@ -7,8 +7,30 @@ function insecureFunction(password) {
   var db = window.openDatabase("myDb", "1.0", "Personal secrets stored here", 2*1024*1024);  // Noncompliant Web SQL database
 
 }
-var userInput = "sensitivePassword";
+let userInput = "sensitivePassword";
 var KEY_TOKEN = "thisisasecrettokenthatnobodyshouldsee";
 var openai_api_key = "password";
+
+// Noncompliant NoSQL operations should not be vulnerable to injection attacks https://rules.sonarsource.com/javascript/type/Vulnerability/RSPEC-5147/
+const { MongoClient } = require('mongodb');
+
+function nosql(req, res) {
+    let query = { user: req.query.user, city: req.query.city };
+
+    MongoClient.connect(url, (err, db) => {
+        db.collection("users")
+        .find(query) // Noncompliant
+        .toArray((err, docs) => { });
+    });
+}
+
+
+
+// Cipher algorithms should be robust https://rules.sonarsource.com/javascript/type/Vulnerability/RSPEC-5547/
+
+const crypto = require('crypto');
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+
+
 
 insecureFunction(userInput);
